3ee95db95ec304ecce0cef2bc9daaf3bb421d6ec
Greg Walker <mgwalker@gmail.com>
2017-02-18 21:37:02 -0600

    Add a pseudoproperty for sorting

--- api/stats/elections.js
+++ api/stats/elections.js
@@ -3,12 +3,16 @@ const util = require('../util');
 module.exports = function electionStats(presidents) {
   return presidents.map((president) => {
     const out = president;
+    out.largestElectoralWin = 0;
     out.elections = out.elections.map((election) => {
       const local = election;
       local.electoralPct = util.round(local.electoralVotes / local.possibleElectoralVotes, 4);
+      if (local.electoralPct > out.largestElectoralWin) {
+        out.largestElectoralWin = local.electoralPct;
+      }
       return local;
     });
-    out.sortBy = 'elections.elecoralPct';
+    out.sortBy = 'largestElectoralWin';
     return out;
   });
 };
